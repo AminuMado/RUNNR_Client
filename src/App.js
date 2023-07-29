a2dd38e4ef28fe7b737df4c1014a5cba52c599f8
Aminu Mukhtar Mado <madoaminu@gmail.com>
2023-07-29 17:41:44 -0400

    moved back to src

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./App.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
+import pizzas from "./data";
+function App() {
+  return <h1>RUNNR LANDING PAGE</h1>;
+}
+
+export default App;
